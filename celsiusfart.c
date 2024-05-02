e3e78cb289a48848db41489ae78484cc10b2c42d
theonelordgod <george88512@gmail.com>
2024-05-02 19:17:50 +0300

    floatintemp

--- celsiusfart.c
+++ celsiusfart.c
@@ -2,7 +2,7 @@
 
 //C=(5/9)(F-32)
 int main() {
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower, upper, step;
 
 	lower = 0;
@@ -11,8 +11,8 @@ int main() {
 
 	fahr = lower;
 	while (fahr <= upper) {
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
+		printf("%3.0f\t%6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
