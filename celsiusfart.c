41d248f9633f432e9a155959ecf26daf11b97771
theonelordgod <george88512@gmail.com>
2024-05-02 22:29:56 +0300

    defines

--- celsiusfart.c
+++ celsiusfart.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+
 //C=(5/9)(F-32)
+//int main() {
+//	float fahr, celsius;
+//	int lower, upper, step;
+//
+//	lower = 0;
+//	upper = 300;
+//	step = 20;
+//
+//	fahr = lower;
+//	printf("F to C");
+//	while (fahr <= upper) {
+//		celsius = (5.0 / 9.0) * (fahr - 32.0);
+//		printf("%3.0f\t%6.1f\n", fahr, celsius);
+//		fahr = fahr + step;
+//	}
+//}
+
 int main() {
-	float fahr, celsius;
-	int lower, upper, step;
+	int fahr;
 
-	lower = 0;
-	upper = 300;
-	step = 20;
-
-	fahr = lower;
-	printf("F to C\n");
-	while (fahr <= upper) {
-		celsius = (5.0 / 9.0) * (fahr - 32.0);
-		printf("%3.0f\t%6.1f\n", fahr, celsius);
-		fahr = fahr + step;
+	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
+		printf("%3d\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 	}
 }
