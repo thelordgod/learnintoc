9f1bbcbf28eb309268dca68e0c967663a6569524
thelordgod <george88512@gmail.com>
2024-05-05 01:23:02 +0300

    exercise 1-13v

--- exercise1_13v.c
+++ exercise1_13v.c
@@ -1,5 +1,47 @@
 #include<stdio.h>
 
+#define MAXWORDLENGTH 32
+
 // print a histogram of the lengths of words in its input (vertical variant)
 int main() {
+	int c, i, j, h, nc, ws;
+
+	int dhist[MAXWORDLENGTH];
+	for (i = 0; i < MAXWORDLENGTH; ++i) {
+		dhist[i] = 0;
+	}
+
+	ws = nc = h = 0;
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\n' || c == '\t') {
+			++ws;
+			if (ws == 1){
+				++dhist[nc];
+				nc = 0;
+			}
+		}
+		else {
+			++nc;
+			ws = 0;
+		}
+	}
+
+	// get histogram height
+	for (i = 0; i < MAXWORDLENGTH; ++i) {
+		if (dhist[i] > h)
+			h = dhist[i];
+	}
+
+	// build histogram
+	for (i = h; i >= 0; --i) {
+		for (j = 1; j < MAXWORDLENGTH; ++j) {
+			if (i == 0)
+				printf("%3d", j);
+			else if (dhist[j] >= i)
+				printf(" = ");
+			else
+				printf("   ");
+		}
+		putchar('\n');
+	}
 }
