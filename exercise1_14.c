c1a13ccd9f50ee1efc10e71fd0773e11197cda66
thelordgod <george88512@gmail.com>
2024-05-05 16:10:33 +0300

    starting exercise 1-14

---
+++ exercise1_14.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+#define ASCIICHARACTERS 256
+
+// print a histogram of the frequecies of different characters in its input
+int main() {
+	int c, i, j, nc;
+
+	int dhist[ASCIICHARACTERS];
+
+	for (i = 0; i < ASCIICHARACTERS; ++i) {
+		dhist[i] = 0;
+	}
+}
