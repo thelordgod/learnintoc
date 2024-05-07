a9c092ddbf28a9c7715b86b0eb8d7455840b4892
theonelordgod <george88512@gmail.com>
2024-05-07 20:10:39 +0300

    starting exercise 1-18 2

--- exercise1_18.c
+++ exercise1_18.c
@@ -4,30 +4,20 @@
 #define MAXLINES 1000
 #define PRINTLENGTH 80
 
-// print all lines longer than 80 characters
+// remove trailing blanks and tabs from each line of input, delete entirely blank lines
 int main() {
 	int c, i, j;
-	int len; // current line length
-	int qlin; // lines that qualify
-	int ll[MAXLINE]; // qualifying line lengths
+	int ll[MAXLINE]; // line lengths
 	char buf[MAXLINE]; // current line buffer
-	char printbuf[MAXLINES][MAXLINE]; // qualifying line buffer
+	char printbuf[MAXLINES][MAXLINE]; // total line buffer
 
 	max = len = qlin = 0;
 	while ((c = getchar()) != EOF) {
 		if (c != '\n') {
 			buf[len] = c;
-			++len;
 		}
-		else if (c == '\n' && len <= PRINTLENGTH)
+	else if (c == '\n' && len <= PRINTLENGTH)
 			len = 0;
-		else if (len > PRINTLENGTH) {
-			for (i = 0; i < len; ++i)
-				printbuf[qlin][i] = buf[i];
-			ll[qlin] = len;
-			++qlin;
-			len = 0;
-		}
 	}
 	for (i = 0; i < qlin; ++i) {
 		for (j = 0; j < ll[i]; ++j)
