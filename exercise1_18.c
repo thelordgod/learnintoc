b29f1c53a1d47307bc353a43b695d9a4e0009563
theonelordgod <george88512@gmail.com>
2024-05-11 19:12:03 +0300

    glory and death but mostly death (1-18 still not done)

--- exercise1_18.c
+++ exercise1_18.c
@@ -3,14 +3,17 @@
 #define MAXLINE 1000
 #define MAXLINES 1000
 #define PRINTLENGTH 80
+#define TRUE 1
+#define FALSE 0
 
 // remove trailing blanks and tabs from each line of input, delete entirely blank lines
 int main() {
-	int c, i, j;
+	int c, i, j, len;
 	int ll[MAXLINE]; // line lengths
 	int qlin; // qualifying line quantity
 	char buf[MAXLINE]; // current line buffer
 	char printbuf[MAXLINES][MAXLINE]; // total line buffer
+	int lwh = FALSE; // last word hit
 
 	len = qlin = 0;
 	while ((c = getchar()) != EOF) {
@@ -18,17 +21,35 @@ int main() {
 			buf[len] = c;
 			++len;
 		}
-		else if (c == '\n') {
-			len = 0;
-		}
 		else {
+			if (len > 0) {
+				for (i = len - 1; i > 0; --i) {
+					printf("%d", len);
+					if (lwh == TRUE) {
+						for (j = 0; j < len; ++j) {
+							printbuf[qlin][j] = buf[j];
+						}
+						ll[qlin] = len;
+						lwh = FALSE;
+					}
+					if (lwh == FALSE && (buf[len] == ' ' || buf[len] == '\t')) {
+						--len;
+						printf("%d", len);
+					}
+					else {
+						lwh = TRUE;
+					}
+				}
+				len = 0;
+				++qlin;
+			}
 		}
 	}
+
 	for (i = 0; i < qlin; ++i) {
 		for (j = 0; j < ll[i]; ++j)
 			printf("%c", printbuf[i][j]);
 		putchar('\n');
 	}
-		
 	return 0;
 }
